862c6ebb32341041598b822929ec7d2ec357a7a7
Goryachev Maksim <gorjachev_maksim@mail.ru>
2018-09-27 23:32:42 +0300

    Make fields

--- src/main/java/ru/goryacms/testmvc/city/model/City.java
+++ src/main/java/ru/goryacms/testmvc/city/model/City.java
@@ -1,4 +1,13 @@
 package ru.goryacms.testmvc.city.model;
 
 public class City {
+    private Long id;
+
+    private String name;
+
+    private String countryCode;
+
+    private String district;
+
+    private Integer population;
 }
